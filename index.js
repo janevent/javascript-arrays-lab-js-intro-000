7ddefa091cb4cb1b3c748c34a73edc559e057475
Jane Venter <janeanneventer@gmail.com>
2018-08-30 04:48:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+}
+function appendKitten(name){
+  return [...kittens, name];
+}
+function prependKitten(name){
+  return [name, ...kittens];
+}
+function removeLastKitten(){
+  var kittens = kittens.slice(-1);
+   return kittens;
+}
 // Add your functions and code here
